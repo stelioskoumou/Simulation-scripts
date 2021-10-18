1bfad120ca6801e0a38c45e3d5bfd8b33e682c64
stelioskoumou <92146497+stelioskoumou@users.noreply.github.com>
2021-10-18 12:45:56 +0300

    Update sim2.py

--- sim2.py
+++ sim2.py
@@ -1,12 +1,12 @@
 from tkinter import *
 root = Tk()
 global sim2_lbl
-sim2_lbl = Label(
+lbl = Label(
     root,
     text="You chose NS-3!",
     fg='black',
     bg='#299617',
     font="Consolas 15 bold"
     )
-sim2_lbl.pack()
+lbl.pack()
 root.mainloop()
