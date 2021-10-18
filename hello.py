c22976b2a38658e4942038fe18378c27ffc132cb
stelioskoumou <92146497+stelioskoumou@users.noreply.github.com>
2021-10-18 11:14:27 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("You chose NS3!")
+from tkinter import *
+root = Tk()
+lbl = Label(
+    root,
+    text="You chose NS-3!",
+    fg="black",
+    bg='#299617',
+    font="Consolas 15 bold"
+    )
+lbl.pack()
+root.mainloop()
