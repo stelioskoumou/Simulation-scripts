819655fad8afa4fc37dfe8f637827aa71e87be8f
stelioskoumou <92146497+stelioskoumou@users.noreply.github.com>
2021-10-18 12:08:18 +0300

    Create sim3.py

---
+++ sim3.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root = Tk()
+lbl = Label(
+    root,
+    text="You chose SUMO!",
+    fg="red",
+    bg='#299617',
+    font="Consolas 15 bold"
+    )
+lbl.pack()
+root.mainloop()
